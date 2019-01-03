83ad381c6824cb85092f8a6575e0b111a3434253
Alperen SALAR <alperenn123@gmail.com>
2019-01-04 01:06:37 +0300

    Arkanoid V1.1

--- src/Arkanoid.cpp
+++ src/Arkanoid.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <time.h>
 #include <sstream>
+#include <iostream>
 #define uint8 uint8_t
 #define int8  int8_t 
 #define int64 int64_t
@@ -11,12 +12,29 @@
 typedef enum
 {
 	Running = 0,
-	Gameover
+	GameOver,
+	Exit
 }GameState;
+typedef struct 
+{
+	GameState eGameState;
+	void Change_State(GameState state)
+	{
+		eGameState = state;
+	}
+	GameState Get_State()
+	{
+		return this->eGameState;
+	}
+}StateMachine;
+ 
+
+ 
 int main()
 {
 	srand(time(0));
-	GameState eGameState = Running;
+	StateMachine stateMachine;
+	stateMachine.eGameState = Running;
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Arkanoid V0.1");
 	window.setFramerateLimit(60);
@@ -46,6 +64,7 @@ int main()
 	sPaddle.setPosition(300, 440);
 	sBall.setPosition(250, 250);
 	sf::Text sScore_Display;
+	sf::Text sGameover_Text;
 	sScore_Display.setFont(fScoreFont);
 	sScore_Display.setCharacterSize(15);
 	sScore_Display.setColor(sf::Color(0, 0, 0));
@@ -73,84 +92,136 @@ int main()
 	float x = 250.0f;
 	float y = 250.0f;
 	sf::Clock clock;
-	float gameover = false;
 
 	
 	while (window.isOpen())
     {
-		switch (eGameState)
+		
+		switch (stateMachine.Get_State())
 		{
 			case Running:
-
-		}
-
-		
-		sf::Event event;
-		sf::Int32 dTime = clock.restart().asMilliseconds();
-		sf::Vector2f ball_pos = sBall.getPosition();
-		if (ball_pos.y < 0)
-			gameover = true;
-		while (window.pollEvent(event))
-		{
-			if(event.type == sf::Event::Closed)
-			window.close();
-		}
-
-		
-		if (ball_pos.x < 0 || ball_pos.x > SCREEN_WIDTH)  dirX = -dirX;
-		if (ball_pos.y < 0) dirY = -dirY;
-		
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) sPaddle.move(6, 0);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) sPaddle.move(-6, 0);
-		if (sf::FloatRect(x + 1, y + 1, 12, 12).intersects(sPaddle.getGlobalBounds()))
-		{
-			//float r =  0.3f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (0.5f - 0.3f)));
-			dirY = -dirY;
-		}
-
-
-		x += dirX * dTime;
-		for (int i = 0; i < 80; ++i)
-		{
-			if (sf::FloatRect(x + 1, y + 1, 12, 12).intersects(sBlocks[i].getGlobalBounds()))
 			{
-				sBlocks[i].setPosition(0, -100);
-				dirX = -dirX;
-				score += 10;
+				window.setKeyRepeatEnabled(true);
+					
 				
-			}
-		}
-		y += dirY * dTime;
-		
-		for (int i = 0; i < 80; ++i)
-		{
-			if (sf::FloatRect(x + 1, y + 1, 12, 12).intersects(sBlocks[i].getGlobalBounds()))
-			{
-				sBlocks[i].setPosition(0, -100);
-				dirY = -dirY;
-				score += 10;
-			}
-		}
+				sf::Event event;
+				
+				sf::Int32 dTime = clock.restart().asMilliseconds();
+				sf::Vector2f ball_pos = sBall.getPosition();
+				while (window.pollEvent(event))
+				{
+					
+					if (event.type == sf::Event::Closed)
+						stateMachine.Change_State(Exit);
+				}
 
-		if (y >= SCREEN_HEIGHT)
-			gameover = true;
-		sBall.setPosition(x, y);
-		dirX += 0.0005f;
-		dirY += 0.0005f;
-		window.clear();
-		window.draw(sBackground);
-		window.draw(sPaddle);
-		window.draw(sBall);
+				if (ball_pos.y > SCREEN_HEIGHT)
+					stateMachine.Change_State(GameOver);
+				else
+				{
+					if (ball_pos.x < 0 || ball_pos.x > SCREEN_WIDTH)  dirX = -dirX;
+					if (ball_pos.y < 0) dirY = -dirY;
+
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) sPaddle.move(6, 0);
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) sPaddle.move(-6, 0);
+					if (sf::FloatRect(x + 1, y + 1, 12, 12).intersects(sPaddle.getGlobalBounds()))
+					{
+
+						dirY = -dirY;
+					}
+
+
+					x += dirX * dTime;
+					for (int i = 0; i < 80; ++i)
+					{
+						if (sf::FloatRect(x + 1, y + 1, 12, 12).intersects(sBlocks[i].getGlobalBounds()))
+						{
+							sBlocks[i].setPosition(0, -100);
+							dirX = -dirX;
+							score += 10;
+
+						}
+					}
+					y += dirY * dTime;
+
+					for (int i = 0; i < 80; ++i)
+					{
+						if (sf::FloatRect(x + 1, y + 1, 12, 12).intersects(sBlocks[i].getGlobalBounds()))
+						{
+							sBlocks[i].setPosition(0, -100);
+							dirY = -dirY;
+							score += 10;
+						}
+					}
+
+					sBall.setPosition(x, y);
+					dirX += 0.0005f;
+					dirY += 0.0005f;
+					window.clear(sf::Color::Black);
+					window.draw(sBackground);
+					window.draw(sPaddle);
+					window.draw(sBall);
+
+					for (int i = 0; i < 80; ++i)
+					{
+						window.draw(sBlocks[i]);
+					}
+					oScore_Stream << score;
+					sScore_Display.setString("Score = " + oScore_Stream.str());
+					oScore_Stream.str("");
+					window.draw(sScore_Display);
+					window.display();
+				}
+				
+				
+			}break;
+			case GameOver:
+			{
+				window.setKeyRepeatEnabled(false);
+				sf::Event event;
+				while (window.pollEvent(event))
+				{
+					switch (event.type)
+					{
+					case sf::Event::Closed: {stateMachine.Change_State(Exit); }break;
+						case sf::Event::KeyPressed: 
+						{
+							if (event.key.code == sf::Keyboard::Enter)
+							{
+								stateMachine.Change_State(Running);
+								sBall.setPosition(150, 150);
+								dirX = 0.2f;
+								dirY = 0.2f;
+								 x = 150.0f;
+								 y = 150.0f;
+							}
+							
+						}break;
+					}
+				}
+				sGameover_Text.setFont(fScoreFont);
+				sGameover_Text.setCharacterSize(30);
+				sGameover_Text.setColor(sf::Color(0, 0, 0));
+				sGameover_Text.setPosition((SCREEN_HEIGHT-150)/2, (SCREEN_WIDTH-150)/2);
+				sGameover_Text.setString("GAMEOVER!Press Enter to restart...");
+				window.draw(sGameover_Text);
+				window.display();
+
+				
+			}break;
+
+			case Exit:
+			{
+				window.close(); 
+			}break;
 		
-		for (int i = 0; i < 80; ++i)
-		{
-			window.draw(sBlocks[i]);
 		}
-		oScore_Stream << score;
-		sScore_Display.setString("Score = " + oScore_Stream.str());
-		oScore_Stream.str("");
-		window.draw(sScore_Display);
-		window.display();
-    }
+		
+
+
+	}
+
+		
+		
     return 0;
 }
